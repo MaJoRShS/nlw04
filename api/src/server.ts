5e87ac318f969c35afcc4d08f873050ab6b6098c
MaJoRShS <lukas.mr@hotmail.com>
2021-02-23 22:22:17 -0300

    Conexao com banco de dados e insert
    Alem da configuraçao que fizemos no banco de dados aind a configuramos praticamente tudo, criamos rotas , repositorys, controllers e ja estamos deixando o server.js bem mais limpo e com tudo separado onde deveiria estar, ia me esquecendo das migrations, que alem de criar um banco de dados top, ainda ajuda a nao ficarmos com o banco de dados desatualizado

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,29 +1,13 @@
+import 'reflect-metadata';
 import express, { request, response } from 'express';
+import "./database" ;
+import { router } from "./routes";
+
 
 const app = express();
 
+app.use(express.json());
+app.use(router);
+
+
 app.listen(3333, ()=> console.log("Server is Running"!));
-
-/**
- * GET => Buscas
- * POST => Salvar
- * PUT => Alterar 
- * DELETE => Deletar
- * PATCH => Alteração especifica
- */
-
-
-
- //Aqui vai ser o caminho do Site mais o / ali em abaixo , assim que nego tentar fizer uma requisição para esse caminho eu vou devolver o que ta meio da função.
-//Método get
-app.get("/", (request, response) => {
-    //Retorno normal com texto normal de resposta
-    // return response.send("Hello World - NLW");
-
-    //Retorno como Json
-    return response.json({message:"Hello World - NLW"});
-});
-
-app.post("/",(request, response) =>{
-    return response.json({message:"Os dados foram salvos com sucesso."});
-});
